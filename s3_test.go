58f716feb9cb710c545f98078e321c5f9845fdc6
Philip Montgomery <pmontgom@broadinstitute.org>
2015-10-31 12:04:33 -0400

    Added check to wait for fakes3 to come online before running tests

--- s3_test.go
+++ s3_test.go
@@ -3,6 +3,7 @@ package singleply
 import (
 	"bytes"
 	"fmt"
+	"time"
 
 	"os/exec"
 
@@ -15,6 +16,9 @@ import (
 
 type S3Suite struct {
 	fakes3Cmd *exec.Cmd
+	svc       *s3.S3
+	endpoint  string
+	region    string
 }
 
 var _ = Suite(&S3Suite{})
@@ -33,6 +37,28 @@ func (s *S3Suite) SetUpSuite(c *C) {
 	if err != nil {
 		panic(err.Error())
 	}
+
+	// wait until fakes3 is responding.  Give it 5 seconds before aborting
+
+	timeout := time.Now().Add(time.Second * 5)
+
+	s.endpoint = "http://localhost:" + S3Port
+	s.region = "us-east-1"
+	config := aws.NewConfig().WithCredentials(credentials.AnonymousCredentials).WithEndpoint(s.endpoint).WithRegion(s.region).WithS3ForcePathStyle(true)
+	s.svc = s3.New(config)
+	for {
+		input := s3.ListBucketsInput{}
+		_, err := s.svc.ListBuckets(&input)
+
+		if err == nil {
+			break
+		}
+
+		now := time.Now()
+		if now.After(timeout) {
+			panic("Timeout waiting for fakes3")
+		}
+	}
 }
 
 func (s *S3Suite) TearDownSuite(c *C) {
@@ -41,16 +67,11 @@ func (s *S3Suite) TearDownSuite(c *C) {
 }
 
 func (s *S3Suite) TestConnection(c *C) {
-	endpoint := "http://localhost:" + S3Port
-	region := "us-east-1"
-
-	config := aws.NewConfig().WithCredentials(credentials.AnonymousCredentials).WithEndpoint(endpoint).WithRegion(region).WithS3ForcePathStyle(true)
-	svc := s3.New(config)
 	createBucket := s3.CreateBucketInput{Bucket: aws.String("bucket")}
-	_, err := svc.CreateBucket(&createBucket)
+	_, err := s.svc.CreateBucket(&createBucket)
 	c.Assert(err, IsNil)
 
-	conn := NewConnection("bucket", "prefix", region, endpoint)
+	conn := NewConnection("bucket", "prefix", s.region, s.endpoint)
 	files, err := conn.ListDir("path", nil)
 	c.Assert(err, IsNil)
 	c.Assert(len(files), Equals, 0)
@@ -60,7 +81,7 @@ func (s *S3Suite) TestConnection(c *C) {
 	putObject.Bucket = aws.String("bucket")
 	putObject.Key = aws.String("prefix/banana")
 	putObject.Body = buffer
-	svc.PutObject(&putObject)
+	s.svc.PutObject(&putObject)
 
 	// putObject = s3.PutObjectInput{}
 	// putObject.Bucket = aws.String("bucket")
@@ -72,13 +93,13 @@ func (s *S3Suite) TestConnection(c *C) {
 	putObject.Bucket = aws.String("bucket")
 	putObject.Key = aws.String("prefix/sampledir/a")
 	putObject.Body = bytes.NewReader(make([]byte, 0))
-	svc.PutObject(&putObject)
+	s.svc.PutObject(&putObject)
 
 	putObject = s3.PutObjectInput{}
 	putObject.Bucket = aws.String("bucket")
 	putObject.Key = aws.String("prefix/sampledir/b")
 	putObject.Body = bytes.NewReader(make([]byte, 0))
-	svc.PutObject(&putObject)
+	s.svc.PutObject(&putObject)
 
 	files, err = conn.ListDir("", nil)
 	fmt.Printf("files=%s\n", files)
